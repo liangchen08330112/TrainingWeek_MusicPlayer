7b8c1747cb3c486b7d4338f4c36f8075e839d08d
liangchen08330112 <3366211746@qq.com>
2022-06-06 11:39:06 +0800

    2022.06.06 11:38梁宸提交。（增设开屏界面，显示2秒后跳转登录页面）

---
+++ app/src/main/java/cn/edu/sict/lc/musicplayer/FirstScreen.java
@@ -0,0 +1,46 @@
+package cn.edu.sict.lc.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import cn.edu.sict.lc.R;
+
+public class FirstScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_screen);
+        AsyncTaskClass taskClass = new AsyncTaskClass();
+        taskClass.execute(2);
+    }
+    class AsyncTaskClass extends AsyncTask<Integer,Integer,Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Integer... integers) {
+            int seconds = integers[0].intValue();
+            while (seconds > 0) {
+                publishProgress(seconds--);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            finish();
+            startActivity(new Intent(FirstScreen.this,LoginActivity.class));
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            if (aBoolean==true){
+                finish();
+                startActivity(new Intent(FirstScreen.this,LoginActivity.class));
+            }
+        }
+    }
+}
